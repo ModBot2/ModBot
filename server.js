d3874f3ec74d44accbd0dec36493fa7ccec5fad5
ModBot <85180831+ModBot2@users.noreply.github.com>
2021-06-01 20:49:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const { default_prefix } = require("./config.json");
 require('dotenv').config();
-const fetch = require("node-fetch");
 const db =require("quick.db");
 const moment = require("moment");
-const { CanvasSenpai } = require("canvas-senpai")
-const canva = new CanvasSenpai();
 const discord = require("discord.js");
 const client = new discord.Client({
   disableEveryone: false
@@ -13,8 +10,6 @@ const yts = require('yt-search')
 
 client.queue = new Map();
 client.vote = new Map();
-const { ready } = require("./handlers/ready.js")
-
 
 require("./uptime.js");
 
@@ -53,83 +48,6 @@ client.on("message", async message => {
   if (command) command.run(client, message, args);
 });
 
-
-client.off("guildMemberAdd", async member => {
-
-  let chx = db.get(`welchannel_${member.guild.id}`);
-
-  if (chx === null) {
-
-    return;
-
-  }
-
-  
-
-   let data = await canva.welcome(member, { link: "https://i.pinimg.com/originals/f3/1c/39/f31c39d56512dc8fbf30f9d0fb3ee9d3.jpg" })
-
- 
-
-    const attachment = new discord.MessageAttachment(
-
-      data,
-
-      "welcome-image.png"
-
-    );
-
-  
-
-  
-
-  client.channels.cache.get(chx).send("Welcome to our Server " + member.user.username, attachment);
-
-});
-
-//chatbot
-
-client.on("message", async message => {
-if (message.channel.name == "chatbot") {
-if (message.author.bot) return;
-message.content = message.content.replace(/@(everyone)/gi, "everyone").replace(/@(here)/gi, "here");
-if (message.content.includes(`@`)) {
-return message.channel.send(`**:x: Please dont mention anyone**`);
- }
-  message.channel.startTyping();
-if (!message.content) return message.channel.send("Please say something.");
-fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${encodeURIComponent(message.content)}&botname=${client.user.username}&ownername=||PASINDU||#0774`)
-    .then(res => res.json())
-    .then(data => {
-        message.channel.send(`> ${message.content} \n <@${message.author.id}> ${data.message}`);
-    });
-      message.channel.stopTyping();
-}
-});
-
-//LEVEL
-
-const { addexp } = require("./handlers/xp.js")
-
-//LEVEL
-client.on("message", async message => {
-if(message.author.bot) return;
-  if(!message.guild) return;
-  
-return addexp(message)
-})
-
-client.snipes = new Map()
-client.on('messageDelete', function(message, channel){
-  
-  client.snipes.set(message.channel.id, {
-    content:message.content,
-    author:message.author.tag,
-    image:message.attachments.first() ? message.attachments.first().proxyURL : null
-  })
-  
-})
- 
-
 const { GiveawaysManager } = require("discord-giveaways");
 // Starts updating currents giveaways
 const manager = new GiveawaysManager(client, {
@@ -142,7 +60,6 @@ const manager = new GiveawaysManager(client, {
         reaction: "🎉"
     }
 });
-// We now have a giveawaysManager property to access the manager everywhere!
 client.giveawaysManager = manager;
 
 client.on("message", async message => {
@@ -154,46 +71,6 @@ if(!message.guild) return;
  
 })
 
-client.on("guildCreate", guild => {
-
-  const { MessageEmbed } = require("discord.js");
-
-  const ID = "812592666286096404";
-
-  const channel = client.channels.cache.get(ID);
-
-  const sowner = guild.owner.user;
-
-  if (!channel) return;
-
-  const embed = new MessageEmbed()
-
-    .setTitle("**I Joined a Server!**")
-
-    .addField(`**SERVER NAME**`, `\`\`\`${guild.name}\`\`\``)
-
-    .addField(`**SERVER ID**`, `\`\`\`${guild.id}\`\`\``)
-
-    .addField(`**SERVER OWNER**`, `\`\`\`${sowner.tag}\`\`\``)
-
-    .addField(`**OWNER ID**`, `\`\`\`${sowner.id}\`\`\``)
- 
-    .addField(`**CREATED ON**`, `\`\`\`${guild.createdAt}\`\`\``)
-  
-    .addField(`**MEMBERS**`, `\`\`\`${guild.memberCount}\`\`\``)
-  
-    .setTimestamp()
-
-    .setColor("32CD32")
-
-    .setFooter(`Servers Count - ${client.guilds.cache.size}`);
-
-  channel.send(embed);
-
-});
-
-
-// Set the bot's online/idle/dnd/invisible status
 client.on("ready", () => {
     client.user.setStatus("online");
     console.log("bot is now online")
